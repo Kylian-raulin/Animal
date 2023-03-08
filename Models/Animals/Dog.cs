f9ac57c99d6c9ce8c65895132d1d920bda09a187
Kylian RAULIN <kylian.raulin@viacesi.fr>
2023-03-08 15:47:03 +0100

    feat(dog): add class dog

---
+++ Models/Animals/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal.Models.Animals
+{
+    class Dog : Animal
+    {
+        public string dogName { get; private set; }
+
+        public Dog(string name)
+        {
+            this.dogName = name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,19 +6,42 @@ using A = Animal.Models;
 
 class Program
 {
+
     static void Main(string[] args)
     {
         Griffin myAnimal1 = new Griffin();
-        
+
         myAnimal1.Sleep();
         myAnimal1.Moove();
         Console.WriteLine(myAnimal1.griffinName);
-      
+
         Dragon myAnimal2 = new Dragon();
         myAnimal2.Moove();
         myAnimal2.Sleep();
 
         Console.WriteLine(myAnimal2.dragonName);
+        
+
+
+        Dog dog1 = new Dog("blue");
+        Dog dog2 = new Dog("red");
+        Dog dog3 = new Dog("snow");
+        Dog dog4 = new Dog("yellow");
+        Dog dog5 = new Dog("purple");
+        Dog[] dogs = { dog1, dog2, dog3, dog4, dog5 };
+
+        IEnumerable<Dog> differentThanSnow =
+            from dog in dogs
+            where dog.dogName != "snow"
+            select dog;
+
+        Console.WriteLine("les chien sont : ");
+        foreach (Dog dog in differentThanSnow)
+        {
+            Console.WriteLine(dog.dogName);
+        }
+
         Console.ReadLine();
     }
+
 }
