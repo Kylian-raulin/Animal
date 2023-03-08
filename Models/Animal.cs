488b486f3a7f5ecc55a932cd2a387048c3ee8e17
Kylian RAULIN <kylian.raulin@viacesi.fr>
2023-03-08 11:23:08 +0100

    feat(class): add class Animal

---
+++ Models/Animal.cs
@@ -0,0 +1,28 @@
+﻿namespace Animal.Models
+{ 
+    public class Animal
+    {
+        public string Name;
+
+        public void Move()
+        {
+            Console.WriteLine($"{Name} il a bougé");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} il dort");
+        }
+
+        public Animal()
+        {
+            this.Name = "baguera";
+        }
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using A = Animal.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A.Animal cat = new A.Animal();
+        A.Animal dog = new A.Animal("toto");
+
+        cat.Move();
+        dog.Sleep();
+
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
